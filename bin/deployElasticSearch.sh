01a8a792df2e81e272bd1dacd798e0f79f4a9f65
Kirill Zabelin <Kirill.Zabelin@kaspersky.com>
2021-10-13 21:17:35 +0300

    Update go.mod and go.sum
    Should go.sum be tracked???

--- bin/deployElasticSearch.sh
+++ bin/deployElasticSearch.sh
@@ -4,3 +4,4 @@ sudo docker pull elasticsearch:7.9.3
 sudo docker pull docker.elastic.co/kibana/kibana:7.9.3
 sudo docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "http.cors.enabled=true" -e "http.cors.allow-origin=http://localhost:8080" -e "discovery.type=single-node" elasticsearch:7.9.3
 sudo docker run -d --link  elasticsearch:elasticsearch  -p 5601:5601   docker.elastic.co/kibana/kibana:7.9.3 
+
